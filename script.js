5c0c855234368b4042127909dd26484c7f4ff03c
Rafal Brzeski <rafal731@gmx.de>
2024-06-30 20:29:50 +0200

    mam gotowca

---
+++ script.js
@@ -0,0 +1,63 @@
+// const flagaPl = document.querySelector('.flaga_pl')
+// const flagaDe = document.querySelector('.flaga_de')
+// const flagaEn = document.querySelector('.flaga_en')
+// const containerDe = document.querySelector('.container_de')
+// const containerEn = document.querySelector('.container_en')
+// const containerPl = document.querySelector('.container_pl')
+// const liElement = document.createElement("li");
+
+
+// const aElement = document.createElement("a");
+
+
+// aElement.setAttribute("href", "./übermich.html");
+
+
+// aElement.textContent = "ABOUTME";
+
+
+// liElement.appendChild(aElement);
+
+
+
+
+
+
+
+// const appearanceDe = () => {
+//     containerDe.style.display = 'block'
+// }
+
+// const disappearDe = () => {
+//     containerDe.style.display = 'none'
+// }
+// const appearanceEn = () => {
+//     containerEn.style.display = 'block'
+// }
+// const disappearEn = () => {
+// 	containerEn.style.display = 'none'
+// }
+
+// const appearancePl = () => {
+//     containerPl.style.display = 'block'
+// }
+// const disappearPl = () => {
+//     containerPl.style.display = 'none'
+// }
+
+// flagaDe.addEventListener('click', appearanceDe)
+// flagaDe.addEventListener('click', disappearEn)
+// flagaDe.addEventListener('click', disappearPl)
+
+
+
+// flagaPl.addEventListener('click', appearancePl)
+// flagaPl.addEventListener('click', disappearDe)
+// flagaPl.addEventListener('click', disappearEn)
+
+// flagaEn.addEventListener('click', appearanceEn)
+// flagaEn.addEventListener('click', disappearDe)
+// flagaEn.addEventListener('click', disappearPl)
+
+// liElement.addEventListener('click', appearanceDe ) 
+// liElement.addEventListener('click', disappearEn ) 
